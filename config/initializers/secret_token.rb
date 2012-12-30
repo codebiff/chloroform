661191c4be374eb1bd20514a69b4c4c87479977d
Dave Nicholson <dave@codebiff.com>
2012-12-30 11:29:51 +0000

    inital commit. rspec, mongo_mapper and capybara setup complete

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chloroform::Application.config.secret_token = '05f10acd75a56ad5cc858a3e3bd42a66cf8682ede53a35151830cf02dfc6420a76809cd96b56c64431897d3d7710d4d2937a9459e741445f6193b931240e19c5'
