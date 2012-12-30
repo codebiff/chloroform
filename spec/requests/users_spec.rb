2b2d24ef962ff2ea8463b475932abc9f87ce7e77
Dave Nicholson <dave@codebiff.com>
2012-12-30 16:40:13 +0000

    some request tests for login

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,24 @@
+require "spec_helper"
+
+describe "Users" do
+
+  def clear_db
+    User.collection.remove
+  end
+
+  context "with valid credentials" do
+
+    it "should login and redirect to the account page" do
+      clear_db
+      visit user_index_path
+      expect {
+        fill_in "email",    with: "joe@example.com"
+        fill_in "password", with: "password"
+        click_button "Login"
+      }.to change(User, :count).by(1)
+    end
+
+  end
+
+  context "with invalid credentials"
+end
