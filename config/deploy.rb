b870225175af3cc94e7097ede58f2ca33f7f830c
codebiff <dave@codebiff.com>
2013-01-07 07:50:13 +0000

    capistrano setup

---
+++ config/deploy.rb
@@ -0,0 +1,26 @@
+require "bundler/capistrano"
+
+set :application, "chloroform"
+set :user, "dave"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+set :scm, "git"
+set :repository, "git@github.com:codebiff/#{application}.git"
+set :branch, "master"
+
+server "codebiff.com", :app, :web, :db, :primary => true
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
